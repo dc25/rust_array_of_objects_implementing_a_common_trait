ba33ebd497da836b72d8870db6f70198175fb74f
dave <dave>
2023-04-18 06:05:16 -0700

    initial

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+trait T { 
+    fn get_name(&self) -> &str;
+}
+
+#[derive(Clone)]
+struct S<'a> { 
+    name: &'a str,
+}
+
+impl<'a> T for S<'a> {
+    fn get_name(&self) -> &'a str {
+        self.name.clone()
+    }
+}
+
+
+fn main() {
+    let s0 = S {name: "xxx"};
+
+    let mut a: Vec<&dyn T> = Vec::new();
+    a.push(&s0);   // borrow
+    for s in a.iter() {
+        println!("{}", s.get_name())
+    }
+    // borrow of s0 ends here because a is no longer used (and implicitly goes out of scope?).
+    // moving the println to the end of the function will cause compile error.
+
+    let mut ab: Vec<Box<dyn T>> = Vec::new();
+    ab.push(Box::new(s0));   // move
+    for s in ab.iter() {
+        println!("{}", s.get_name())
+    }
+
+}
